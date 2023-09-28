0087e65edb857ddbb659700dc7567655bf25ab09
Testers7777 <Tiagodemingo@gmail.com>
2023-09-28 22:16:34 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+tippy('.discord', {
+    content: 'discord',
+    theme: "type",
+    offset: [0, 12],
+    arrow: false,
+});
+tippy('.github', {
+    content: 'github',
+    theme: "type",
+    offset: [0, 12],
+    arrow: false,
+});
